f1be816f2f18fe87ea07dc13a481e21cc298b81d
Viktoria Artemiak <145024364+ToriArtemiak@users.noreply.github.com>
2024-04-15 23:05:32 +0300

    Create pattern_matching.py

---
+++ src/pattern_matching.py
@@ -0,0 +1,52 @@
+from typing import List, Dict
+def build_automaton(needle_value: str) -> List[Dict[str, int]]:
+    automaton = [dict() for _ in range(len(needle_value) + 1)]
+    m = len(needle_value)
+
+    for i in range(m):
+        for char in automaton[i]:
+            if char == needle_value[i]:
+                automaton[i][char] = i + 1
+            else:
+                j = i
+                while j > 0:
+                    j = automaton[j - 1].get(char, 0)
+                    if char == needle_value[j]:
+                        automaton[i][char] = j + 1
+                        break
+                if j == 0:
+                    automaton[i][char] = 0
+
+    return automaton
+
+
+def search(needle_value: str, haystack_value: str) -> List[int]:
+    automaton = build_automaton(needle_value)
+    m, n = len(needle_value), len(haystack_value)
+
+    j = 0
+    indices = []
+
+    for i in range(n):
+        while j > 0 and haystack_value[i] != needle_value[j]:
+            j = automaton[j - 1].get(haystack_value[i], 0)
+
+        if haystack_value[i] == needle_value[j]:
+            j += 1
+
+        if j == m:
+            indices.append(i - m + 1)
+            j = automaton[j - 1].get(haystack_value[i], 0)
+
+    return indices
+
+
+if __name__ == "__main__":
+    haystack = str(input("Введіть <<haystack>>: "))
+    needle = str(input("Введіть <<needle>>: "))
+
+    result = search(needle, haystack)
+    if result:
+        print(f"Індекси входжень підстрічки '{needle}' в стрічці '{haystack}': {result}")
+    else:
+        print(f"Підстрічка '{needle}' не знайдена в стрічці '{haystack}'")
