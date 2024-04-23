f392eec8c5845e782f006c74c588471c1c988ae7
Viktoria Artemiak <145024364+ToriArtemiak@users.noreply.github.com>
2024-04-23 09:44:38 +0300

    Update and rename pattern_matching.py to fsm_pattern_search.py

---
+++ src/fsm_pattern_search.py
@@ -0,0 +1,39 @@
+def build_atomaton(needle_value: str) -> list:
+    table = [{} for _ in range(len(needle_value) + 1)]
+    for i in range(len(needle_value)):
+        char = needle_value[i]
+        for state in range(i + 1):
+            if char not in table[state]:
+                table[state][char] = i + 1
+            else:
+                table[state][char] = max(table[state][char], i + 1)
+    return table
+
+
+def search(needle_value: str, haystack_value: str) -> list:
+    transition_table = build_atomaton(needle_value)
+    current_state = 0
+    indices = []
+
+    for i, char in enumerate(haystack_value):
+        if char in transition_table[current_state]:
+            current_state = transition_table[current_state][char]
+        else:
+            current_state = 0
+        if current_state == len(needle_value):
+            indices.append(i - len(needle_value) + 1)  
+            current_state = transition_table[current_state - 1].get(needle_value[-1], 0)  
+
+    indices = [index for index in indices if index >= 0]
+    return indices
+
+
+if __name__ == "__main__":
+    haystack_value = str(input("Введіть <<haystack>>: "))
+    needle_value = str(input("Введіть <<needle>>: "))
+
+    result = search(needle_value, haystack_value)
+    if result:
+        print(f"Індекси входжень підстрічки '{needle_value}' в стрічці '{haystack_value}': {result}")
+    else:
+        print(f"Підстрічка '{needle_value}' не знайдена в стрічці '{haystack_value}'")
