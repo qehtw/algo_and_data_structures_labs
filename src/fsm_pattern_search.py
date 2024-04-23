c94b0def80b2da4f78eed4cc58b798d3c71487a1
Viktoria Artemiak <145024364+ToriArtemiak@users.noreply.github.com>
2024-04-23 10:05:55 +0300

    Update fsm_pattern_search.py

--- src/fsm_pattern_search.py
+++ src/fsm_pattern_search.py
@@ -1,4 +1,4 @@
-def build_atomaton(needle_value: str) -> list:
+def build_automaton(needle_value: str) -> list:
     table = [{} for _ in range(len(needle_value) + 1)]
     for i in range(len(needle_value)):
         char = needle_value[i]
@@ -9,20 +9,33 @@ def build_atomaton(needle_value: str) -> list:
                 table[state][char] = max(table[state][char], i + 1)
     return table
 
-
 def search(needle_value: str, haystack_value: str) -> list:
-    transition_table = build_atomaton(needle_value)
+    transition_table = build_automaton(needle_value)
     current_state = 0
     indices = []
+    haystack_length = len(haystack_value)
 
-    for i, char in enumerate(haystack_value):
+    i = 0
+    while i < haystack_length:
+        char = haystack_value[i]
         if char in transition_table[current_state]:
             current_state = transition_table[current_state][char]
         else:
             current_state = 0
         if current_state == len(needle_value):
-            indices.append(i - len(needle_value) + 1)  
-            current_state = transition_table[current_state - 1].get(needle_value[-1], 0)  
+            indices.append(i - len(needle_value) + 1)
+            current_state = transition_table[current_state - 1].get(needle_value[-1], 0)
+        i += 1
 
     indices = [index for index in indices if index >= 0]
     return indices
+
+if __name__ == "__main__":
+    haystack_value = str(input("Введіть <<haystack>>: "))
+    needle_value = str(input("Введіть <<needle>>: "))
+
+    result = search(needle_value, haystack_value)
+    if result:
+        print(f"Індекси входжень підстрічки '{needle_value}' в стрічці '{haystack_value}': {result}")
+    else:
+        print(f"Підстрічка '{needle_value}' не знайдена в стрічці '{haystack_value}'")
